829be2ff44821dbec1b28f957c7a834fdfbc69c7
Jarmo Pertman <jarmo@jarmopertman.com>
2022-10-08 17:59:16 +0300

    Apply shellcheck fixes

--- tinify.sh
+++ tinify.sh
@@ -2,10 +2,10 @@
 
 set -Eeo pipefail
 
-IMAGE_PATHS="$@"
+IMAGE_PATHS=("$@")
 
-if [[ -z "$IMAGE_PATHS" ]]; then 
-  echo "Usage: "$0" IMAGE..."
+if [[ -z "${IMAGE_PATHS[0]}" ]]; then 
+  echo "Usage: $0 IMAGE..."
   exit 1
 fi
 
@@ -14,24 +14,24 @@ if [[ -z "$TINIFY_API_KEY" ]]; then
   exit 1
 fi
 
-for IMAGE_PATH in ${IMAGE_PATHS[@]}; do
-  for IMAGE in $(find "$IMAGE_PATH" -name "*.jpg" -o -name "*.png" -o -name "*.webp"); do
+for IMAGE_PATH in "${IMAGE_PATHS[@]}"; do
+  find "$IMAGE_PATH" \( -name "*.jpg" -o -name "*.png" -o -name "*.webp" \) -print0 | while IFS= read -r -d '' IMAGE; do
     echo
     echo "Processing $IMAGE"
 
-    if echo "$IMAGE" | grep -qE "\-opt\..+$"; then
-      echo "Already optimized, skipping"
+    if echo "$IMAGE" | grep -qE "\-tin\..+$"; then
+      echo "Already tinified, skipping"
       continue
     fi
 
-    OPTIMIZED_IMAGE_URL="$(curl -s --user api:"$TINIFY_API_KEY" --data-binary @$IMAGE https://api.tinify.com/shrink | jq -r '.output.url')"
+    TINIFIED_IMAGE_URL="$(curl -s --user api:"$TINIFY_API_KEY" --data-binary @"$IMAGE" https://api.tinify.com/shrink | jq -r '.output.url')"
 
     IMAGE_FILE_NAME="$(basename "$IMAGE")"
     IMAGE_FILE_NAME_WITHOUT_EXT="${IMAGE_FILE_NAME%.*}"
     IMAGE_FILE_NAME_EXT="${IMAGE_FILE_NAME##*.}"
-    NEW_IMAGE_PATH="$(dirname "$IMAGE")/$IMAGE_FILE_NAME_WITHOUT_EXT-opt.$IMAGE_FILE_NAME_EXT"
+    NEW_IMAGE_PATH="$(dirname "$IMAGE")/$IMAGE_FILE_NAME_WITHOUT_EXT-tin.$IMAGE_FILE_NAME_EXT"
 
-    echo "Saving optimized image to $NEW_IMAGE_PATH"
-    curl -so "$NEW_IMAGE_PATH" "$OPTIMIZED_IMAGE_URL"
+    echo "Saving tinified image to $NEW_IMAGE_PATH"
+    curl -so "$NEW_IMAGE_PATH" "$TINIFIED_IMAGE_URL"
   done
 done
